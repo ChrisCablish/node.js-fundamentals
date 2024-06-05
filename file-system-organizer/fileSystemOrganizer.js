053229296285f19713df496185b75079eae03920
ChrisCablish <112437010+ChrisCablish@users.noreply.github.com>
2024-06-05 08:41:00 -0500

    implement new path creation and file moving functionality

--- file-system-organizer/fileSystemOrganizer.js
+++ file-system-organizer/fileSystemOrganizer.js
@@ -2,12 +2,11 @@ import fs from "fs";
 import path from "path";
 import { fileURLToPath } from "url";
 
+//name the target directory
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const targetDirectory = path.join(__dirname, "target");
 
-// now that we have the directory where we want to look for files, we need to actually read them and
-// do something with them.
-
+//read the target directory
 fs.readdir(
   targetDirectory,
   { encoding: "utf-8", withFileTypes: true },
@@ -16,11 +15,36 @@ fs.readdir(
       console.error("Failed to read directory:", err);
       return;
     }
-
-    console.log(files[0].isFile());
     console.log(
       "Files in directory:",
       files.map((file) => file.name)
     );
+
+    //once the directory is read, loop through each file
+    files.forEach((file) => {
+      if (file.isDirectory()) {
+        console.log(`${file.name} is a directory`); //don't create a new directory if it already exists
+      } else {
+        const ext = path.extname(file.name); //get file extension
+        const extDirectory = path.join(targetDirectory, ext || "no ext"); //create new directory path string (not the path itself)
+
+        if (!fs.existsSync(extDirectory)) {
+          fs.mkdirSync(extDirectory, { recursive: true }); //this actually creates the new path
+        }
+
+        //name the old and new file path strings to pass into fs.rename
+        const oldFilePath = path.join(targetDirectory, file.name);
+        const newFilePath = path.join(extDirectory, file.name);
+
+        //move the file(s) to new extension specific directory using fs.rename
+        fs.rename(oldFilePath, newFilePath, (err) => {
+          if (err) {
+            console.error(`Failed to move file: ${file.name}`, err);
+            return;
+          }
+          console.log(`Moved ${file.name} to ${extDirectory}/`);
+        });
+      }
+    });
   }
 );
